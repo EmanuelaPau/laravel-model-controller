8331dbcc9d5ab5e85e99a2aebbe2bb7de58e8ebd
EmanuelaPau <uapame@gmail.com>
2023-07-20 16:04:41 +0200

    Add movies cards with foreach

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 @section('title','Homepage')
-@section('main-content')
+@section('main-section')
     <h1>
         Homepage Movies
     </h1>
+    {{-- @dd($movies) --}}
+
+    @foreach ($movies as $movie )
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{$movie['title']}}</h5>
+                <p class="card-text">{{$movie['original_title']}}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Nationality: <strong>{{$movie['nationality']}}</strong></li>
+                <li class="list-group-item">Date: <strong>{{$movie['date']}}</strong> </li>
+                <li class="list-group-item">Vote: <strong>{{$movie['vote']}}</strong> </li>
+            </ul>
+        </div>
+    @endforeach
 @endsection
