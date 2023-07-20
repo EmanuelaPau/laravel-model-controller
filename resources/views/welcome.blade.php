47059d70f64334d53743e33c55b2a065086477c4
Emanuela Pau <121491102+EmanuelaPau@users.noreply.github.com>
2023-07-20 15:07:00 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title','Welcome')
+
+@section('main-section')
+    <h1>
+        Welcome
+    </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>
+                    Ciaone
+                </h3>
+            </div>
+        </div>
+    </div>
+@endsection
